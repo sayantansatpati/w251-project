afc31ce0d1be53335ce6221803bb45ad6806425d
Ryan Chamberlain <ryanchamberlain@invenshure.com>
2015-06-03 20:21:11 -0500

    Finalized EC2 to SoftLayer data transfer script

--- load_enron_data.py
+++ load_enron_data.py
@@ -1,4 +1,8 @@
+"""
+Run on an EC2 instance with the Enron data mounted as an EBS volume.
+"""
 import os
+import itertools
 
 import swiftclient
 
@@ -8,4 +12,19 @@ key = os.environ['SWIFT_KEY']
 
 conn = swiftclient.client.Connection(
         authurl=authurl, user=user, key=key)
-print conn.head_container('w251-enron')
+
+root_dir = '/enron'
+folders = [os.path.join(root_dir, folder) for folder in os.listdir(root_dir) if 'enron' in folder]
+
+folder = folders[0]
+files = [
+          [os.path.join(folder, filename) for filename in os.listdir(folder)]
+          for folder in folders
+        ]
+files = itertools.chain(*files)
+
+for filename in files:
+    with open(filename, 'r') as fp:
+        print 'Uploading %s' % filename
+        conn.put_object(container='w251-enron', obj=filename, contents=fp, chunk_size=512000)
+
